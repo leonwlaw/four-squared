f909dde0d1d14bc61bcd6d5c29ea38bdc68a4593
Leonard Law <leon.wlaw@gmail.com>
2014-03-14 21:18:27 -0400

    Central scoring piece

---
+++ Assets/scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	const float colorChangeFactor = 0.2f;
+	const float minimumColorDifference = 0.001f;
+	const int floorsLayer = 1 << 8;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject ball = GameObject.Find("Ball");
+		RaycastHit floor;
+		// Gradually change the color of the indicator to match the
+		// floor that the ball is on.
+		if (Physics.Raycast(ball.transform.position, Vector3.down, out floor, Mathf.Infinity, floorsLayer)) {
+			Color colorDifference = floor.collider.gameObject.renderer.material.color - renderer.material.color;
+			Vector3 difference = (Vector3)(Vector4)colorDifference;
+
+			// Make sure colors change by a constant amount.
+			difference.Normalize();
+			difference *= colorChangeFactor * Time.deltaTime;
+			if (difference.magnitude > minimumColorDifference) {
+				renderer.material.color += (Color)(Vector4)difference;
+			} else {
+				renderer.material.color = floor.collider.gameObject.renderer.material.color;
+			}
+		}
+	}
+}
