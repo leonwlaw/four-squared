820948cac3d57b6dc8c8c1afd4f1f5c1182f4638
Johnny He <jhe05@students.poly.edu>
2014-03-26 16:46:39 -0400

    Change Scenes, Intro, Game, Instructions, End

--- Assets/scripts/Instructions.cs
+++ Assets/scripts/Instructions.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class Instructions : MonoBehaviour {
 
+	public KeyCode keyToPush; // assign in inspector
+	public string levelToLoad; // assign in inspector
+	float elapsed;
+
 	// Use this for initialization
 	void Start () {
-	
+		elapsed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		elapsed += Time.deltaTime;
+		if (elapsed > 0.1f && Input.GetKeyDown (keyToPush)) {
+			Debug.Log (levelToLoad);
+			Application.LoadLevel (levelToLoad);
+			elapsed = 0;
+		}
 	}
 }
